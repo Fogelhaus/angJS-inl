a91d11a92e12fbc471163c5a3d9671464a9a750e
Dennis Nilsson <dennis@thedennis.se>
2019-04-29 00:13:40 +0200

    inl4 klar

---
+++ restapi/routes/products.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const route = express.Router();
+
+route.get('/', (req, res, next) => {
+    res.status(200).json([
+        { productname: "Apple iPhone 7 32GB", category: "Mobile Telephone", company: "Apple", shortdescription: "32GB Black", price: 4299 , rating: "4", instock : 100, instockdate: new Date("2019-03-05")  },
+        { productname: "Apple iPhone 7 64GB", category: "Mobile Telephone", company: "Apple", shortdescription: "64GB Black", price: 4799 , rating: "5", instock : 100, instockdate: new Date("2019-03-05")  },
+        { productname: "Apple iPhone 8 64GB", category: "Mobile Telephone", company: "Apple", shortdescription: "64GB Black", price: 5299 , rating: "5", instock : 100, instockdate: new Date("2019-03-05")  },
+        { productname: "Apple iPhone 8 128GB", category: "Mobile Telephone", company: "Apple", shortdescription: "128GB Black", price: 5999 , rating: "5", instock : 100, instockdate: new Date("2019-03-05")  },
+        { productname: "Apple iPad 4", category: "Surfplatta", company: "Apple", shortdescription: "128GB Black", price: 4999 , rating: "5", instock : 10, instockdate: new Date("2019-03-26")  },
+        { productname: "Apple iPhone X", category: "Mobile Telephone", company: "Apple", shortdescription: "128GB Black", price: 6299 , rating: "5", instock : 100, instockdate: new Date("2019-03-05")  },
+        { productname: "Apple iPhone XS", category: "Mobile Telephone", company: "Apple", shortdescription: "128GB Black", price: 6399 , rating: "5", instock : 100, instockdate: new Date("2019-03-05")  },
+        { productname: "Apple iPhone X+", category: "Mobile Telephone", company: "Apple", shortdescription: "256GB Black", price: 6999 , rating: "5", instock : 100, instockdate: new Date("2019-03-05")  },
+        { productname: "HP Probook 640", category: "Laptop Computer",  company: "HP", shortdescription: "Core i5 8GB 256GB SSD 14 tum", price: 6799 , rating: "5", instock : 100, instockdate: new Date("2019-03-04")  },
+        { productname: "Voxicon D27QO IPS", category: "Monitor",  company: "Voxicon", shortdescription: "27tum 2560 x 1440 16:9", price: 1595 , rating: "2", instock : 23, instockdate: new Date("2019-03-01")  },
+        { productname: "Dell UltraSharp U2417H Infinity", category: "Monitor",  company: "Dell", shortdescription: "23.8tum 1920 x 1080 16:9", price: 1899 , rating: "4", instock : 49, instockdate: new Date("2019-03-02")  },
+        { productname: "HP 250 G6", category: "Laptop Computer",  company: "HP", shortdescription: "Core i5 8GB 256GB SSD 15.6tum", price: 4599 , rating: "5", instock : 1, instockdate: new Date("2019-03-02")  },
+        { productname: "Logitech MX Master for Business", category: "Mouse & Keyboard",  company: "Logitech", shortdescription: "Laser 1600dpi Mouse Black", price: 100 , rating: "5", instock : 0, instockdate: new Date("2019-03-01")  },
+        { productname: "Lenovo ThinkCentre M710q Tiny", category: "Desktop Computer", company: "Lenovo", shortdescription: "Core i5 8GB 256GB SSD", price: 4799 , rating: "4", instock : 0, instockdate: new Date("2019-03-04")  },
+        { productname: "Apple iPhone 6S", category: "Mobile Telephone", company: "Apple", shortdescription: "64GB Space Grey", price: 5599 , rating: "5", instock :44, instockdate: new Date("2019-03-02")  },
+        { productname: "Intel Core i9 9900K", category: "Processor",  company: "Intel", shortdescription: "3.6GHz LGA1151 Socket", price: 4899 , rating: "3", instock : 99, instockdate: new Date("2019-03-05")  },
+        { productname: "Logitech MX Master 2S", category: "Mouse & Keyboard", company: "Logitech", shortdescription: "Laser 4000dpi Mouse Grey", price: 899 , rating: "5", instock : 100, instockdate: new Date("2019-03-01")  }
+    ]);    
+});
+
+module.exports = route;
--- script/app.js
+++ script/app.js
@@ -10,59 +10,72 @@ var app = angular
     })
 
 
-    .controller('mainController', function ($scope) {
-        $scope.products = products;
-
-        // Antal produkter per sida
-        $scope.productsShow = 10;
-        $scope.productsTotal = $scope.products.length;
-
-        //sortering och sök
-        $scope.sortColumn = "productname";
-        $scope.reversesort = false;
-
-        $scope.sortData = function (column) {
-            //RESULTAT KAN HAMNA UTANFÖR SYNLIGA LISTAN VID REVERSESORT
-            // $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
-            $scope.sortColumn = column;
-        }
-
-        $scope.getSortClass = function (column) {
-
-            //      if ($scope.sortColumn == column) {
-            //          return $scope.reverseSort ? 'down-arrow' : 'up-arrow';
-            //    }
-            //    return ''; 
-        }
-
-        $scope.search = function (item) {
-            if ($scope.searchText == undefined) {
-                return true;
+    .factory("productsGet", function ($http) {
+        return {
+            products: function () {
+                return $http.get("http://localhost:5000/api/products").then(function (response) {
+                    return response.data;
+                });
             }
-            else {
-                if (
-                    item.company.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1 ||
-                    item.productname.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1 ||
-                    item.category.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1
-                ) {
+        }
+    })
+
+    .controller('mainController', function ($scope, productsGet) {
+
+        productsGet.products().then(function (data) {
+            $scope.products = data;
+
+            // Antal produkter per sida
+            $scope.productsShow = 10;
+            $scope.productsTotal = $scope.products.length;
+
+            //sortering och sök
+            $scope.sortColumn = "productname";
+            $scope.reversesort = false;
+
+            $scope.sortData = function (column) {
+                //RESULTAT KAN HAMNA UTANFÖR SYNLIGA LISTAN VID REVERSESORT
+                // $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
+                $scope.sortColumn = column;
+            }
+
+            // $scope.getSortClass = function (column) {
+
+            //           if ($scope.sortColumn == column) {
+            //               return $scope.reverseSort ? 'down-arrow' : 'up-arrow';
+            //         }
+            //        return ''; 
+            // }
+
+            $scope.search = function (item) {
+                if ($scope.searchText == undefined) {
                     return true;
                 }
+                else {
+                    if (
+                        item.company.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1 ||
+                        item.productname.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1 ||
+                        item.category.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1
+                    ) {
+                        return true;
+                    }
+                }
+                return false;
             }
-            return false;
-        }
 
-        //Grid eller listvy
-        $scope.cardActive = true;
+            //Grid eller listvy
+            $scope.cardActive = true;
 
-        //Stjärnor i rating
-        var ratingMax = 5;
+            //Stjärnor i rating
+            var ratingMax = 5;
 
-        $scope.getRepeater = function () {
-            return new Array(ratingMax);
-        };
+            $scope.getRepeater = function () {
+                return new Array(ratingMax);
+            };
 
-        $scope.getNumber = function(num){
-            return new Array(num);
-          }  
+            $scope.getNumber = function (num) {
+                return new Array(num);
+            }
 
-    });
+        });
+    })
