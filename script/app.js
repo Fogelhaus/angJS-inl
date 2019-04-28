b3eb84c92da824cf926ddc381399c872c78a9558
Dennis Nilsson <dennis@thedennis.se>
2019-04-28 16:05:40 +0200

    inl3 klar

--- script/app.js
+++ script/app.js
@@ -1,41 +1,68 @@
 var app = angular
     .module('mainApp', ['ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider.when("/", {
+            templateUrl: "partials/list.view.html"
+        })
+            .when("/viewGrid", {
+                templateUrl: "partials/grid.view.html"
+            })
+    })
 
 
-    .controller('mainController', function($scope){
+    .controller('mainController', function ($scope) {
         $scope.products = products;
+
+        // Antal produkter per sida
         $scope.productsShow = 10;
         $scope.productsTotal = $scope.products.length;
 
+        //sortering och sök
         $scope.sortColumn = "productname";
         $scope.reversesort = false;
 
-        $scope.sortData = function(column) {
-            $scope.reverseSort = ($scope.sortColumn == column) ?  !$scope.reverseSort : false;
+        $scope.sortData = function (column) {
+            //RESULTAT KAN HAMNA UTANFÖR SYNLIGA LISTAN VID REVERSESORT
+            // $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
             $scope.sortColumn = column;
-
         }
-            $scope.getSortClass = function(column) {
-                
-                if ($scope.sortColumn == column) {
-                    return $scope.reverseSort ? 'down-arrow' : 'up-arrow';
-                }
-                return '';
+
+        $scope.getSortClass = function (column) {
+
+            //      if ($scope.sortColumn == column) {
+            //          return $scope.reverseSort ? 'down-arrow' : 'up-arrow';
+            //    }
+            //    return ''; 
+        }
+
+        $scope.search = function (item) {
+            if ($scope.searchText == undefined) {
+                return true;
             }
-            $scope.search = function(item) {
-                if($scope.searchText == undefined) {
+            else {
+                if (
+                    item.company.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1 ||
+                    item.productname.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1 ||
+                    item.category.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1
+                ) {
                     return true;
                 }
-                else {
-                    if(
-                        item.company.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1 ||
-                        item.productname.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1 ||
-                        item.category.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1
-                    ) {
-                        return true;
-                    }
-                }
-
-                return false;
             }
+            return false;
+        }
+
+        //Grid eller listvy
+        $scope.cardActive = true;
+
+        //Stjärnor i rating
+        var ratingMax = 5;
+
+        $scope.getRepeater = function () {
+            return new Array(ratingMax);
+        };
+
+        $scope.getNumber = function(num){
+            return new Array(num);
+          }  
+
     });
